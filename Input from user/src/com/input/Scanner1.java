6775ce715bbb878a9522f2a6688cfde3a19cc380
Roma844 <rgvyas13@gmail.com>
2015-10-08 14:32:45 +0530

    committed.

---
+++ Input from user/src/com/input/Scanner1.java
@@ -0,0 +1,23 @@
+package com.input;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+	static  Scanner s= new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		System.out.println("enter roll_no");
+		int n= s.nextInt();
+		System.out.println("Enter name");
+		String name= s.next();
+		//float f= s.nextFloat();
+		System.out.println("Enter salary");
+		float f= s.nextFloat();
+		System.out.println(n);
+		System.out.println(name);
+		System.out.println(f);
+		//scanner class is not compulsory to close.. we can or can't
+		s.close();
+	}
+
+}
